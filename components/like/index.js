877f098d54e4845a0cbbe4c865bd56be81626ae8
momo <xirizhifeng@163.com>
2019-12-16 16:32:34 +0800

    applet demo

---
+++ components/like/index.js
@@ -0,0 +1,31 @@
+// pages/components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    like:{
+      type:Boolean,
+    },
+    count:{
+      type:Number
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    yesSrc:"images/like.png",
+    noSrc:"images/like@dis.png"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLike:function(event){
+      console.log(event)
+    }
+  }
+})
