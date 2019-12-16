93e84ce06127d8883f77083ae6c81688aba4e1b8
momo <xirizhifeng@163.com>
2019-12-16 16:57:18 +0800

    like component

--- components/like/index.js
+++ components/like/index.js
@@ -4,11 +4,11 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    like:{
-      type:Boolean,
+    like: {
+      type: Boolean,
     },
-    count:{
-      type:Number
+    count: {
+      type: Number
     }
   },
 
@@ -16,16 +16,23 @@ Component({
    * 组件的初始数据
    */
   data: {
-    yesSrc:"images/like.png",
-    noSrc:"images/like@dis.png"
+    yesSrc: "images/like.png",
+    noSrc: "images/like@dis.png"
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    onLike:function(event){
-      console.log(event)
+    onLike: function(event) {
+      let like = this.properties.like
+      let count = this.properties.count
+
+      count = like ? count - 1 : count + 1
+      this.setData({
+        count: count,
+        like: !like
+      })
     }
   }
 })
