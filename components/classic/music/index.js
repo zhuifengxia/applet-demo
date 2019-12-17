0a25197b9e2c46dc72264d5210e140cf9023a72c
momo <xirizhifeng@163.com>
2019-12-17 17:31:29 +0800

    music play

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,21 +1,47 @@
 import { classicBeh } from "../classic-beh.js";
+
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   /**
    * 组件的属性列表
    */
   behaviors: [classicBeh],
-  properties: {},
+  properties: {
+    src:String
+  },
 
+  detached:function(event){
+    mMgr.stop()
+  },
   /**
    * 组件的初始数据
    */
   data: {
-    pauseSrc: "images/player@waitting.png",
-    playSrc: "images/player@playing.png"
+    playing:false,
+    pauseSrc: "images/player@pause.png",
+    playSrc: "images/player@play.png"
   },
 
   /**
    * 组件的方法列表
    */
-  methods: {}
+  methods: {
+    onPlay:function(event){
+      //图片切换
+      if(!this.data.playing){
+        this.setData({
+          playing:true
+        })
+        mMgr.title = this.properties.src
+        mMgr.src = this.properties.src
+      }else{
+        this.setData({
+          playing:false
+        })
+        mMgr.pause()
+      }
+      
+    }
+  }
 });
