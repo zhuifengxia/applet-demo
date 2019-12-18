476658d7ccf669b0a7875c616cbb53c866c98bf1
momo <xirizhifeng@163.com>
2019-12-18 17:32:43 +0800

    book detail module

---
+++ pages/book-detail/book-detail.js
@@ -0,0 +1,91 @@
+import { BookModel } from "../../models/book.js";
+import { LikeModel } from "../../models/like.js";
+const bookModel = new BookModel();
+const likeModel = new LikeModel();
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    comments: [],
+    book: null,
+    likeStatus: false,
+    likeCount: 0,
+    posting: false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    const bid = options.bid;
+
+    const detail = bookModel.getDetail(bid);
+    const comments = bookModel.getComments(bid);
+    const likeStatus = bookModel.getLikeStatus(bid);
+    detail.then(res => {
+      this.setData({
+        book: res
+      });
+    });
+    comments.then(res => {
+      this.setData({
+        comments: res.comments
+      });
+    });
+    likeStatus.then(res => {
+      this.setData({
+        likeStatus: res.like_status,
+        likeCount: res.fav_nums
+      });
+    });
+  },
+  onLike(event) {
+    const like_or_cancel = event.detail.behavior;
+    likeModel.like(like_or_cancel, this.data.book.id, 400);
+  },
+  onFakePost(event) {
+    this.setData({
+      posting: true
+    });
+  },
+  onCancel(event) {
+    this.setData({
+      posting: false
+    });
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {}
+});
--- util/http.js
+++ util/http.js
@@ -33,15 +33,16 @@ class HTTP {
     });
   }
 
-  _show_error(error_code){
-      if(!error_code){
-        error_code=1
-      }
+  _show_error(error_code) {
+    if (!error_code) {
+      error_code = 1;
+    }
+    const tip=tips[error_code]
     wx.showToast({
-        title:tips[error_code],
-        icon:'none',
-        duration:2000
-    })
+      title: tip?tip:tips[1],
+      icon: "none",
+      duration: 2000
+    });
   }
 }
 
