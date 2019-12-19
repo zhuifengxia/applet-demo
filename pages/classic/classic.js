cfd2972738e81363395bd5f4b869c92121815c09
momo <xirizhifeng@163.com>
2019-12-19 17:08:43 +0800

    my module finish

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,60 +1,60 @@
-import {ClassicModel} from '../../models/classic.js'
-import {LikeModel} from '../../models/like.js'
-let classicModel = new ClassicModel()
-let likeModel = new LikeModel()
+import { ClassicModel } from "../../models/classic.js";
+import { LikeModel } from "../../models/like.js";
+let classicModel = new ClassicModel();
+let likeModel = new LikeModel();
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    classic:null,
-    latest:true,
-    first:false,
-    likeCount:0,
-    likeStatus:false
+    classic: null,
+    latest: true,
+    first: false,
+    likeCount: 0,
+    likeStatus: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    classicModel.getlatest((res)=>{
+    classicModel.getlatest(res => {
       this.setData({
         //...res 这种写法在页面上不需要classic.image  直接image即可获取数据
-        classic:res,
-        likeCount:res.fav_nums,
-        likeStatus:res.like_status
-      })
-    })
+        classic: res,
+        likeCount: res.fav_nums,
+        likeStatus: res.like_status
+      });
+    });
   },
-  onLike:function(event){
-    let behavior=event.detail.behavior
-    likeModel.like(behavior,this.data.classic.id,this.data.classic.type)
+  onLike: function(event) {
+    let behavior = event.detail.behavior;
+    likeModel.like(behavior, this.data.classic.id, this.data.classic.type);
   },
-  onNext:function(event){
-    this._updateClassic('next')
+  onNext: function(event) {
+    this._updateClassic("next");
   },
-  onPrevious:function(event){
-    this._updateClassic('previous')
+  onPrevious: function(event) {
+    this._updateClassic("previous");
   },
-  _updateClassic:function(nextOrPrevious){
-    let index=this.data.classic.index
-    classicModel.getClassic(index,nextOrPrevious,(res)=>{
-      this._getLikeStatus(res.id,res.type)
+  _updateClassic: function(nextOrPrevious) {
+    let index = this.data.classic.index;
+    classicModel.getClassic(index, nextOrPrevious, res => {
+      this._getLikeStatus(res.id, res.type);
       this.setData({
-        classic:res,
-        latest:classicModel.isLatest(res.index),
-        first:classicModel.isFirst(res.index)
-      })
-    })
+        classic: res,
+        latest: classicModel.isLatest(res.index),
+        first: classicModel.isFirst(res.index)
+      });
+    });
   },
-  _getLikeStatus:function(artID,category){
-    likeModel.getClassicLikeStatus(artID,category,(res)=>{
+  _getLikeStatus: function(artID, category) {
+    likeModel.getClassicLikeStatus(artID, category, res => {
       this.setData({
-        likeCount:res.fav_nums,
-        likeStatus:res.like_status
-      })
-    })
+        likeCount: res.fav_nums,
+        likeStatus: res.like_status
+      });
+    });
   },
 
   /**
